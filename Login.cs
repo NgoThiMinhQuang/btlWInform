c63bf2f74457bb2fdde56016ab0e7f0157d00fea
HuyLuan21 <nguyenluan03122005@gmail.com>
2024-12-15 21:56:42 +0700

    login first commit

---
+++ Login.cs
@@ -0,0 +1,23 @@
+namespace Phone_Store
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void inkForgotPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
